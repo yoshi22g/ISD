31bed8a62000b3970eb1ec0e4c4fc9865f937712
Ojus Doshi <ojus.doshi@gmail.com>
2017-03-04 15:53:33 -0500

    css modifications
    added some styles

--- functions.php
+++ functions.php
@@ -2,6 +2,14 @@
 
 function idc_setup() {
 	//prepare theme for translation
-	load_child_theme_textdomain('idc', get_stylesheet_director() . '/languages');
+	load_child_theme_textdomain('idc', get_stylesheet_directory() . '/languages');
 }
 add_action('after_setup_theme','idc_setup');
+
+function idc_scripts() {
+	wp_dequeue_style( 'popper-fira-sans' );
+	wp_dequeue_style( 'popper-merriweather' );
+}
+add_action('wp_enqueue_scripts','idc_scripts');
+
+?>
