c9100ef0015a166f271be17f75cf9d40f7a0c3b1
Ojus Doshi <ojus.doshi@gmail.com>
2017-03-04 22:53:46 -0500

    news changes
    Modified several php files, and css to format the News section

--- functions.php
+++ functions.php
@@ -12,4 +12,38 @@ function idc_scripts() {
 }
 add_action('wp_enqueue_scripts','idc_scripts');
 
+
+/**
+ * Prints HTML with meta information for post-date/time and author on index pages.
+ */
+function popper_index_posted_on() {
+
+	$author_id = get_the_author_meta( 'ID' );
+
+	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+	}
+
+	$time_string = sprintf( $time_string,
+		esc_attr( get_the_date( 'c' ) ),
+		esc_html( get_the_date() ),
+		esc_attr( get_the_modified_date( 'c' ) ),
+		esc_html( get_the_modified_date() )
+	);
+
+	$posted_on = ('<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+	);
+
+	$byline = sprintf(
+		esc_html_x( 'by %s', 'post author', 'idc' ),
+		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+	);
+
+	echo '<div class="meta-content">';
+	echo '<span class="posted-on">' . $posted_on . ' </span>'; // WPCS: XSS OK.
+	echo '</div><!-- .meta-content -->';
+
+}
+
 ?>
