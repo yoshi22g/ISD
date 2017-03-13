686777bd3845ee0f52a55fdb33c74f70efe6fcd6
Ojus Doshi <ojus.doshi@gmail.com>
2017-03-12 22:59:27 -0400

    srcset

--- js/idc-scripts.js
+++ js/idc-scripts.js
@@ -18,7 +18,8 @@ jQuery(document).ready(function( $ ) {
 //// Jquery Image Duotone ////
    var $slideImage = '.slick-image-slide img';
 
-   $($slideImage).duotone({gradientMap: '#FF3250, #FFFFFF'}).duotone();
+   $($slideImage).attr("srcset",'');
+   $($slideImage).duotone({gradientMap: '#FF3250, #FFFFFF'});
 
    /*$($slideImage).mouseenter(function(e) {
       console.log('mouse enter slide image');
