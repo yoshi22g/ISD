eaec01d63b5984515248f14a9a1947e30170ed46
Ojus Doshi <ojus.doshi@gmail.com>
2017-03-13 05:55:15 -0400

    fuck

--- header.php
+++ header.php
@@ -80,7 +80,7 @@
 		<?php if ($dir == 'research'): ?>
 			<?php echo do_shortcode('[slick-slider category="5" design="design-3" arrows="false" sliderheight="419" speed="1000" autoplay_interval="4000"]'); ?>
 			<div class="research-caption">
-				<a href=""><img id="link-arrow" src="<?php get_stylesheet_directory_uri() ?>/images/chevron_right_black.svg"></a>
+				<a href=""><img id="link-arrow" src="<?php echo get_stylesheet_directory_uri() ?>/images/chevron_right_black.svg"></a>
 				<h3 class="research-caption-title"> Research Title </h3>
 				<p class="research-caption-text">Research caption</p>
 			</div>
