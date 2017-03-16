fc2c1eed2790d39c2d8001cfb1ce28715b7b57eb
Ojus Doshi <ojus.doshi@gmail.com>
2017-03-16 02:51:03 -0400

    more tweaks

--- header.php
+++ header.php
@@ -79,7 +79,7 @@
 	</header><!-- #masthead -->
 		<?php if ($dir == 'research'): ?>
 			<?php echo do_shortcode('[slick-slider category="5" design="design-3" arrows="false" sliderheight="419" speed="1000" autoplay_interval="4000"]'); ?>
-			<div class="research-caption">
+			<div class="research-caption min">
 				<img id="link-arrow" class="closed" src="<?php echo get_stylesheet_directory_uri() ?>/images/chevron_right_black.svg">
 				<h3 class="research-caption-title"> Research Title </h3>
 				<p class="research-caption-text hidden">Research caption</p>
